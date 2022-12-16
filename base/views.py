164ca7056e8283e566eb0de20cc1ef0271f28a80
fboomax <fboomax@gmail.com>
2022-12-16 16:47:17 +0200

    create base app
    Create home, recipe,recipes in views
    templates for views
    and the base urls

---
+++ base/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+
+total_recipes = [
+    {'id': 1, 'Name': 'Gemista', 'Category': 'Ladera', 'Difficulty': 'Medium', 'Total-minutes': 30, 'Total-steps': 2,
+     'Main-photo': 'photo-main', },
+    {'id': 2, 'Name': 'Traxanas', 'Category': 'Ladera', 'Difficulty': 'Medium', 'Total-minutes': 30, 'Total-steps': 2,
+     'Main-photo': 'photo-main', },
+    {'id': 3, 'Name': 'Pastitio', 'Category': 'Ladera', 'Difficulty': 'Medium', 'Total-minutes': 30, 'Total-steps': 2,
+     'Main-photo': 'photo-main', },
+]
+step_recipe = [
+    {'id': 1, 'title': 'Mageirema kima', 'Description': 'Vazooume ligo nero sto ..', 'Step-duration': 30,
+     'Photo': 'photo1', 'Process-bar': 30},
+    {'id': 2, 'title': 'Mageirema kima', 'Description': 'Vazooume ligo nero sto ..', 'Step-duration': 30,
+     'Photo': 'photo1', 'Process-bar': 30}
+]
+
+ingredients = [
+    {'id': 1, 'ingredient': 'kima', 'Photo': 'photo1'},
+    {'id': 2, 'ingredient': 'kima', 'Photo': 'photo1'},
+    {'id': 3, 'ingredient': 'kima', 'Photo': 'photo1'},
+]
+
+
+def home(request):
+    context = {'stepRecipes': step_recipe}
+    return render(request, 'base/home.html', context)
+
+
+def recipes(request):
+    context = {'trecipes': total_recipes}
+    return render(request, 'base/recipes.html', context)
+
+
+def recipe(request, pk):
+    context = {'recipes': recipes}
+    return render(request, 'base/recipe.html', context)
