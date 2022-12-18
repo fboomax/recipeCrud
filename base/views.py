05c940ffd856e23243b1760b38938987af278201
Foivos <60961840+fboomax@users.noreply.github.com>
2022-12-18 12:38:08 +0200

    Create app (#1)
    * create base app
    
    Create home, recipe,recipes in views
    templates for views
    and the base urls
    
    * views.py add in recipe each on view
    
    recipe.htm chage h1 to name of recipe
    
    * change the python & postgres version in docker to reduce the size
    
    add in docker-compose the postgres credentials
    add uth user
    
    * Creating 3 pages
    
    1st home go to recipes
    2nd recipes go to recipe
    3nd recipe view
    Create logic for these pages with static data

---
+++ base/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+
+total_recipes = [
+    {'id': 1, 'name': 'Gemista', 'category': 'Ladera', 'difficulty': 'Medium', 'totalMinutes': 30, 'totalSteps': 2,
+     'mainPhoto': 'photo-main', },
+    {'id': 2, 'name': 'Traxanas', 'category': 'Ladera', 'difficulty': 'Medium', 'totalMinutes': 30, 'totalSteps': 2,
+     'mainPhoto': 'photo-main', },
+    {'id': 3, 'name': 'Pastitio', 'category': 'Ladera', 'difficulty': 'Medium', 'totalMinutes': 30, 'totalSteps': 2,
+     'mainPhoto': 'photo-main', },
+]
+step_recipe = [
+    {'id': 1, 'title': 'mageirema kima', 'Description': 'vazooume ligo nero sto ..', 'step-duration': 30,
+     'Photo': 'photo1', 'Process-bar': 30},
+    {'id': 2, 'title': 'mageirema kima', 'Description': 'vazooume ligo nero sto ..', 'step-duration': 30,
+     'photo': 'photo1', 'process-bar': 30}
+]
+
+ingredients = [
+    {'id': 1, 'ingredient': 'kima', 'Photo': 'photo1'},
+    {'id': 2, 'ingredient': 'kima', 'Photo': 'photo1'},
+    {'id': 3, 'ingredient': 'kima', 'Photo': 'photo1'},
+]
+
+
+def home(request):
+    context = {'stepRecipes': step_recipe}
+    return render(request, 'base/home.html', context)
+
+
+def recipes(request):
+    context = {'trecipes': total_recipes}
+    print(context)
+    return render(request, 'base/recipes.html', context)
+
+
+def recipe(request, pk):
+    selectedRecipe = None
+    for i in total_recipes:
+        if i['id'] == int(pk):
+            selectedRecipe = i
+    context = {'selectedRecipe': selectedRecipe}
+    print(context)
+    return render(request, 'base/recipe.html', context)
