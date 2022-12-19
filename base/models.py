a1b07bc937acc6f7a4acdfa87427d20715fe47dd
fboomax <fboomax@gmail.com>
2022-12-19 15:01:14 +0200

    models.py creating the blueprint for StepRecipe and Ingredient

--- base/models.py
+++ base/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 
-
-
 class Recipe(models.Model):
     name = models.CharField(max_length=80)
     category = models.CharField(max_length=40)
@@ -33,3 +31,21 @@ class Recipe(models.Model):
 
     def __str__(self):
         return self.name
+
+# class StepRecipe(models.Model):
+#     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+#     step = models.IntegerField()
+#     title = models.CharField(max_length=80)
+#     # description=models.TextField()
+#     # ingredient
+#     duration = models.FloatField(validators=[MinValueValidator(0.0)])
+#     image = models.ImageField(upload_to='media')
+#     # progressBar
+#
+#     updated = models.DateTimeField(auto_now=True)
+#     created = models.DateTimeField(auto_now_add=True)
+
+
+# class Ingredient(models.Model):
+#     name = models.CharField(max_length=80)
+#     stepRecipe = models.ForeignKey(StepRecipe, on_delete=models.CASCADE)
