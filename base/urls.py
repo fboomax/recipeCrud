ad6a5251909a231a8d920209f0feee8a35065bee
Foivos <60961840+fboomax@users.noreply.github.com>
2022-12-23 11:07:58 +0200

    views.py add update&delete Recipe def (#5)

--- base/urls.py
+++ base/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('recipes/', views.recipes, name='recipes'),
     path('recipe/<str:pk>/', views.recipe, name='recipe'),
     path('create-recipe/', views.createRecipe, name='create-recipe'),
+    path('update-recipe/<str:pk>', views.updateRecipe, name='update-recipe'),
+    path('delete-recipe/<str:pk>', views.deleteRecipe, name='delete-recipe'),
 ]
--- base/views.py
+++ base/views.py
@@ -51,3 +51,23 @@ def createRecipe(request):
         #     return redirect('recipes')
     context = {'form': form}
     return render(request, 'base/recipe_form.html', context)
+
+def updateRecipe(request, pk):
+    recipe = Recipe.objects.get(id=pk)
+    form = RecipeForm(instance=recipe)
+    if request.method=="POST":
+        form = RecipeForm(request.POST, instance=recipe)
+        if form.is_valid():
+            form.save()
+            return redirect('recipes')
+    context = {'form':form}
+    return render(request, 'base/recipe_form.html', context)
+
+
+def deleteRecipe(request, pk):
+    recipe = Recipe.objects.get(id=pk)
+    if request.method == 'POST':
+        recipe.delete()
+        return redirect('recipes')
+    return render(request, 'base/delete.html', {'obj': recipe})
+
